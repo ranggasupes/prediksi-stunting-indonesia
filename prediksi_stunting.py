11ab936139d27740b0e68f5947384bc01fc17220
Kenzo <ranggasupes@gmail.com>
2026-06-16 00:55:01 +0800

    Melakukan Scaling Data untuk SVR

--- prediksi_stunting.py
+++ prediksi_stunting.py
@@ -44,3 +44,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     random_state=42
 )
 
+scaler = StandardScaler()
+
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
